580ddb881a3d4d303febef947c3166ea8d5ef283
yuslook <alex.yusef@icloud.com>
2026-09-20 19:50:39 -0400

    Remove unused NativeBridge imports

--- lib/native_bridge.dart
+++ lib/native_bridge.dart
@@ -1,32 +1,26 @@
 // lib/native_bridge.dart
-import 'dart:async';
-import 'dart:convert';
-
 import 'package:flutter/services.dart';
-import 'package:flutter/foundation.dart';
 import 'services/db_service.dart';
 
 class NativeBridge {
-  static const MethodChannel _channel =
-      MethodChannel('com.mipypos.app/logic');
+  static const MethodChannel _channel = MethodChannel('com.mipypos.app/logic');
 
-  /// Debe llamarse temprano desde `main()` después de inicializar servicios (Hive, DBService, etc.)
   static void setup() {
     _channel.setMethodCallHandler((call) async {
       switch (call.method) {
         case 'getProductsCount':
           try {
-            final products = await DBService.getProducts();
-            return products.length;
-          } catch (e) {
+            return (await DBService.getProducts()).length;
+          } catch (_) {
             return 0;
           }
         case 'ping':
           return 'pong';
         default:
           throw PlatformException(
-              code: 'NOT_IMPLEMENTED',
-              message: 'Method not implemented: ${call.method}');
+            code: 'NOT_IMPLEMENTED',
+            message: 'Method not implemented: ${call.method}',
+          );
       }
     });
   }
